d797c77435d41bc817fee7a786986b1abc5ea98f
Vincent <106967729+vincelemke99@users.noreply.github.com>
2024-05-10 17:30:26 +0200

    Delete Tutorial directory

--- Tutorial/script.js
+++
@@ -1,8 +0,0 @@
-function appendOperation(operation){
-    document.getElementById("resultArea").innerHTML += operation;
-}
-function calculateResult(){
-    let container = document.getElementById('resultArea')
-    let result = eval(container.innerHTML)
-    container.innerHTML = result;
-}
